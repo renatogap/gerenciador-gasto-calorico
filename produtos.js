9b78b8f2466c6c4e4ebd26b7ed9e217df0f365e1
Renato Pereira <renato.19gp@gmail.com>
2022-08-24 00:16:13 -0300

    Add files via upload

---
+++ produtos.js
@@ -0,0 +1,101 @@
+const listaProdutos = [
+    {
+        id: 1,
+        nome: "Pão Francês",
+        quantidade: 1,
+        medida: "UNID",
+        calo: 140,
+        prot: 4.73,
+        carbo: 25.33,
+        gord: 1.98,
+        value: ""
+    },
+    {
+        id: 2,
+        nome: "Ovo Frito (grande)",
+        quantidade: 1,
+        medida: "UNID",
+        calo: 89,
+        prot: 6.24,
+        carbo: 0.43,
+        gord: 6.76,
+        value: ""
+    },
+    {
+        id: 3,
+        nome: "Café com leite",
+        quantidade: 1,
+        medida: "UNID",
+        calo: 82,
+        prot: 2,
+        carbo: 14,
+        gord: 1.9,
+        value: ""
+    },
+    {
+        id: 4,
+        nome: "Banana",
+        quantidade: 80,
+        medida: "G",
+        calo: 71.20,
+        prot: 0,
+        carbo: 18.40,
+        gord: 0,
+        value: ""
+    },
+    {
+        id: 5,
+        nome: "3Whey Nitro2",
+        quantidade: 40,
+        medida: "G",
+        calo: 150,
+        prot: 31,
+        carbo: 3,
+        gord: 1.6,
+        value: ""
+    },
+    {
+        id: 6,
+        nome: "Arroz Branco",
+        quantidade: 80,
+        medida: "G",
+        calo: 103,
+        prot: 2.13,
+        carbo: 22.32,
+        gord: 0.22,
+        value: ""
+    },
+    {
+        id: 7,
+        nome: "Frango",
+        quantidade: 180,
+        medida: "G",
+        calo: 351,
+        prot: 53.19,
+        carbo: 0,
+        gord: 13.9,
+        value: ""
+    },
+    {
+        id: 8,
+        nome: "Tapioca",
+        quantidade: 60,
+        medida: "G",
+        calo: 145,
+        prot: 0,
+        carbo: 36,
+        gord: 0,
+        value: ""
+    },
+    {
+        id: 9,
+        nome: "Batata Inglesa",
+        quantidade: 50,
+        medida: "G",
+        calo: 41,
+        prot: 1,
+        carbo: 10,
+        gord: 0.6,
+        value: ""
+    }
+]
